3124c7fe9a63d1745659a09d88dad9518c693fe9
luismasuelli <luismasuelli@hotmail.com>
2015-04-22 09:21:05 -0500

    Version 0.0.5 - Puse bien una migracion que dependa de la inicial de contenttypes ya que de la segunda no es necesaria y ademas rompe en django==1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-dynsettings-ritual',
-    version='0.0.4',
+    version='0.0.5',
     namespace_packages=['grimoire', 'grimoire.django'],
     packages=find_packages(exclude=['dynsettings_proj', 'dynsettings_proj.*']),
     package_data={
