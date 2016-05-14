ad614f17218ebf04fad71bf1c4a6881c17f5936f
luismasuelli <luismasuelli@hotmail.com>
2016-05-13 22:34:17 -0500

    Version 0.0.6 - Permitimos version mayor de polymorphic, asi como permitimos version mayor de django

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-dynsettings-ritual',
-    version='0.0.5',
+    version='0.0.6',
     namespace_packages=['grimoire', 'grimoire.django'],
     packages=find_packages(exclude=['dynsettings_proj', 'dynsettings_proj.*']),
     package_data={
@@ -15,5 +15,5 @@ setup(
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='A Django application used to store dynamic settings (i.e. settings beyond the settings.py file), and retrieve them via another special object (instead of django.conf.settings, and wrapping it).',
-    install_requires=['python-cantrips>=0.6.6', 'Django>=1.7', 'jsonfield>=1.0.3', 'django-polymorphic==0.7']
+    install_requires=['python-cantrips>=0.6.6', 'Django>=1.7', 'jsonfield>=1.0.3', 'django-polymorphic>=0.7']
 )
